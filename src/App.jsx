d180c8323c2c714660dd19266f173ecf99b52d7f
M <41431476+marlybone@users.noreply.github.com>
2023-02-03 22:27:44 +0000

    styled navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import Navbar from './Components/Navbar'
 
 export default function App() {
   return (
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  </head>
-    
-
-  )
+    <div>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+        <Navbar />
+    </div>
+  );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,26 +1,49 @@
 import React from 'react'
 import Styled from 'styled-components'
 
-const Navbar = styled.nav`
+
+const Nav = Styled.nav`
+  display: flex;
+  flex-direction: row;
+  top: 0;
+  width: 100%;
+  transition: all 0.2s ease-in-out;
   
+  @media(max-width: 748px){
+    justify-content: flex-end;
+    display: flex;
+    position: sticky;
+    font-size: 14px;
+    
+  }
 `;
 
-const NavItems = styled.ul`
+const NavItems = Styled.ul`
 
+    @media(max-width: 748px){
+  }
+`;
+
+const NavLink = Styled.a`
+  text-decoration: none;
+  padding: 10px;
+  margin: 0 auto;
+  align-items: center;
+  color: black;
+  position: sticky;
 `;
 
 const Navbar = () => {
   return(
     <header>
-      <Navbar>
+      <Nav>
          <NavItems>
-            <a className="items" href="#home"> Home </a>
-            <a className="items" href="#about"> About </a>
-            <a className="items" href="#projects"> Projects </a>
-            <a className="items" href="#contact"> Contact </a>
-
+            <NavLink href='#'>Home</NavLink>
+            <NavLink href='#'>About</NavLink>
+            <NavLink href='#'>Projects</NavLink>
+            <NavLink href='#'>Clontact</NavLink>
           </NavItems>
-      </Navbar>
+      </Nav>
     </header>
   )
 };
