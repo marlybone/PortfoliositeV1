7eab73898775157679256e8539d0deb548fa7020
M <41431476+marlybone@users.noreply.github.com>
2023-02-03 22:40:22 +0000

    mobile view seems ok

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import Styled from 'styled-components'
+import { useState } from 'react'
 
 
 const Nav = Styled.nav`
   display: flex;
-  flex-direction: row;
+  flex-direction: row-reverse;
   top: 0;
   width: 100%;
-  transition: all 0.2s ease-in-out;
+  transition: all 0.4s ease-in-out;
+  position: sticky
   
   @media(max-width: 748px){
     justify-content: flex-end;
     display: flex;
-    position: sticky;
     font-size: 14px;
     
   }
@@ -28,12 +29,15 @@ const NavLink = Styled.a`
   text-decoration: none;
   padding: 10px;
   margin: 0 auto;
-  align-items: center;
   color: black;
-  position: sticky;
+
+  @media(max-width: 748px){
+  align-items: center;
+  }
 `;
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return(
     <header>
       <Nav>
