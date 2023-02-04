f045d9523b42aed0823e420064132fec2d260977
M <41431476+marlybone@users.noreply.github.com>
2023-02-04 13:51:30 +0000

    fixing the navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,26 +2,32 @@ import React from 'react'
 import Styled from 'styled-components'
 import { useState } from 'react'
 
+const NavButton = Styled.button`
+  border: 2px solid red; 
+`;
 
 const Nav = Styled.nav`
   display: flex;
   flex-direction: row-reverse;
-  top: 0;
   width: 100%;
   transition: all 0.4s ease-in-out;
-  position: sticky
   
-  @media(max-width: 748px){
-    justify-content: flex-end;
+@media (max-width: 748px) {
+    justify-content: flex-start;
     display: flex;
     font-size: 14px;
-    
+    &.open {
+      transform: translateX(0%);
+    }
+
+    &.closed {
+      transform: translateX(100%);
+    }
   }
 `;
 
 const NavItems = Styled.ul`
-
-    @media(max-width: 748px){
+    @media(min-width: 748px){
   }
 `;
 
@@ -32,16 +38,17 @@ const NavLink = Styled.a`
   color: black;
 
   @media(max-width: 748px){
-  align-items: center;
+  
   }
 `;
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return(
-    <header>
+    <header>  
       <Nav>
-         <NavItems>
+        <NavButton onClick={() => setIsOpen(!isOpen)}/>
+         <NavItems style={{display: isOpen ? 'block' : 'none'}}>
             <NavLink href='#'>Home</NavLink>
             <NavLink href='#'>About</NavLink>
             <NavLink href='#'>Projects</NavLink>
