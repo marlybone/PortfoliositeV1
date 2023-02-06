693fdf8175f011f3e8ab0cf052c0708e5eec1a62
M <41431476+marlybone@users.noreply.github.com>
2023-02-06 20:40:19 +0000

    Content mobile view is mostly complete.

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,42 +1,59 @@
 import React from 'react'
 import Styled from 'styled-components';
 
-   const ContentDiv = Styled.div`
-     margin-top: 25%;
-     color:white;
+const ContentDiv = Styled.div`
+   margin-top: 25%;
+   margin-bottom: 25%;
+   color:white;
 
-     @media (max-width: 748px){
-     align-items: center;
-     align-content: center;
-     display: flex;
-     flex-direction: column;
+   @media (max-width: 748px){
+   align-items: center;
+   align-content: center;
+   display: flex;
+   flex-direction: column;
 }
 
 img {
-  width: 100%;
-  max-width: 240px;
-  height: auto;
-  max-height: 325px;
   transition: all 0.5s ease-in-out;
   
-    @media (max-width: 748px){
-    }
-}
-h1 {
-
   @media (max-width: 748px){
-   font-size: 50px;
+    width: 100%;
+    max-width: 265px;
+    height: auto;
+    max-height: 355px;
     }
 }
-h2 {
-  @media (max-width: 748px) {
-   display: flex;
-   align-self: flex-start;
-   margin: 0 auto;
-  }
+
+h1 {
+ @media (max-width: 748px){
+  font-size: 38px;
+    }
 }
-     
+
+h2 {
+ @media (max-width: 748px){
+  display: flex;
+  margin: 0;
+  font-size: 32px;
+    }
+}
+
+h3 {
+ @media (max-width: 748px){
+  display: flex;
+  margin: 0;
+  font-size: 18px;
+    }
+}
+
+p {
+ @media (max-width: 748px){
+   padding: 10px;
+   font-size: 14px;
+    }
+}
     `;
+
 
 const Content = () => {
   return (
