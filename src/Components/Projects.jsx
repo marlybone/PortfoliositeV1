7a9165bb4e0de6ee52f724a765f43de59eaa2198
M <41431476+marlybone@users.noreply.github.com>
2023-02-06 20:52:18 +0000

    Start on the projects page

---
+++ src/Components/Projects.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Styled from 'styled-components'
+
+const GridContainer = Styled.div`
+ display: grid;
+
+ @media (max-width: 748px){
+ 
+ }
+`;
+
+
+const Projects = () => {
+
+  return(
+    <GridContainer>
+      
+    
+    </GridContainer>
+  );
+}
+
+export default Projects
